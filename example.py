be71bdc86186cd78d9f9c0e67f0ef8af091d81e5
akritsingha <64059503+akritsingha@users.noreply.github.com>
2020-07-05 01:20:35 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,49 @@
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(400, 400)
+        MainWindow.setMinimumSize(QtCore.QSize(400, 400))
+        MainWindow.setMaximumSize(QtCore.QSize(400, 400))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(100, 60, 211, 81))
+        font = QtGui.QFont()
+        font.setPointSize(28)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.open = QtWidgets.QPushButton(self.centralwidget)
+        self.open.setGeometry(QtCore.QRect(100, 210, 191, 61))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.open.setFont(font)
+        self.open.setObjectName("open")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 400, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "TextLabel"))
+        self.open.setText(_translate("MainWindow", "Open DialogBox"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
